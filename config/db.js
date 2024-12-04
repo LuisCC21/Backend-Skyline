6da96f38da69012319ca9c051d0c76891c4e17a6
LuisCC21 <carcamocariasl@yahoo.com>
2024-12-04 13:18:25 -0600

    new config

--- config/db.js
+++ config/db.js
@@ -3,7 +3,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const sequelize = new Sequelize("postgresql://postgres:TNIKMGGTIndAMbGQIfcRgDGJMwqWmiml@postgres.railway.internal:5432/railway", {
+
+
+export const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   dialectOptions: {
     ssl: {
