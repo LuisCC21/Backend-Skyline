a73104c01451c5982b9ca7cbeed220c359e73c2e
LuisCC21 <carcamocariasl@yahoo.com>
2024-12-04 11:10:43 -0600

    confing production DB

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+export const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   dialectOptions: {
     ssl: {
