4ba15b1a7f3f6e439fd1f107e950ec798c2a4d77
LuisCC21 <carcamocariasl@yahoo.com>
2024-12-04 10:53:31 -0600

    confing production DB

--- config/db.js
+++ config/db.js
@@ -1,10 +1,16 @@
 import { Sequelize } from "sequelize";
-
 import dotenv from "dotenv";
-dotenv.config()
-export const sequelize = new Sequelize(process.env.DATABASE_URL, {
+
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
- 
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 const connectDb = async () => {
