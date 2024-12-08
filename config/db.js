f8613f2e7ee9e28b9aa22e4da2628f8afd8b443d
LuisCC21 <carcamocariasl@yahoo.com>
2024-12-07 23:03:20 -0600

    formateo de codigo

--- config/db.js
+++ config/db.js
@@ -5,12 +5,7 @@ dotenv.config();
 
 export const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
+ 
 });
 
 const connectDb = async () => {
